6758eb1edb870bf2d2d3431b016a81553f06e5dd
sen866 <sen866@gmail.com>
2015-01-25 12:01:18 +0900

    NGUIに依存していた箇所修正

---
+++ sources/model/utility/Updater.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace action_game.sources.model.utility
+{
+    static public class Updater
+    {
+        static public event Action<float> OnUpdate = delegate { };
+
+        static public event Action<float> OnFixedUpdate = delegate { };
+
+        static public event Action<float> OnLastUpdate = delegate { };
+
+        static public void Update(float deltaTime)
+        {
+            OnUpdate(deltaTime);
+        }
+
+        static public void FixedUpdate(float deltaTime)
+        {
+            OnFixedUpdate(deltaTime);
+        }
+
+        static public void LastUpdate(float deltaTime)
+        {
+            OnLastUpdate(deltaTime);
+        }
+    }
+}
